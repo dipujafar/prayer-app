4fb48975e6acee973627e9d7aa1dddb306d70417
unknown <dipujafarmr@gmail.com>
2024-09-05 20:57:57 +0600

    fix a iusse

--- src/app/(with-layout)/setting/privacy-policy/page.tsx
+++ src/app/(with-layout)/setting/privacy-policy/page.tsx
@@ -1,23 +1,31 @@
 "use client"
 import { Button, ConfigProvider } from 'antd';
 import { useState } from 'react';
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
-import './quill.css'
+import './quill.css';
 
-// Dynamically import ReactQuill with SSR disabled
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+// Dynamically import ReactQuill with no SSR
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 const PrivacyPolicy = () => {
     const [value, setValue] = useState('');
     const handleEditorText = () => {
-        console.log(value)
+        console.log(value);
     };
+
     return (
         <div>
-
             <h1 className=' text-2xl font-bold mb-6 text-white'>Privacy Policy</h1>
-            <ReactQuill className=' bg-white min-h-[600px] border-none rounded-md' theme="snow" value={value} onChange={setValue} placeholder='Start writing......' />;
+
+            {/* Ensure ReactQuill only renders on the client */}
+            <ReactQuill 
+                className='bg-white min-h-[600px] border-none rounded-md' 
+                theme="snow" 
+                value={value} 
+                onChange={setValue} 
+                placeholder='Start writing......' 
+            />
 
             <ConfigProvider
                 theme={{
@@ -29,16 +37,18 @@ const PrivacyPolicy = () => {
                             defaultHoverColor: "white",
                             colorBgTextActive: "white",
                             colorPrimaryActive: "white",
-
                         },
                     },
                 }}
             >
-                <Button onClick={handleEditorText} className=' bg-secondary text-white py-6 text-lg border-none hover:bg-secondary hover:text-white' block>Save Changes</Button>
+                <Button 
+                    onClick={handleEditorText} 
+                    className='bg-secondary text-white py-6 text-lg border-none hover:bg-secondary hover:text-white' 
+                    block
+                >
+                    Save Changes
+                </Button>
             </ConfigProvider>
-            <div>
-                
-            </div>
         </div>
     );
 };
