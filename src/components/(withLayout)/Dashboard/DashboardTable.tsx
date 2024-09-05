64e83a12ae9a0b14d72d73853828e8939fad4297
unknown <dipujafarmr@gmail.com>
2024-09-05 19:38:16 +0600

    fix some error

--- src/components/(withLayout)/Dashboard/DashboardTable.tsx
+++ src/components/(withLayout)/Dashboard/DashboardTable.tsx
@@ -1,7 +1,4 @@
-
 import { ConfigProvider, Table } from "antd";
-
-
 
 const DashboardTable = () => {
 
@@ -12,100 +9,98 @@ const DashboardTable = () => {
         email: string;
         date: string;
     }[] = [
-            {
-                key: '1',
-                serial: '1',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '2',
-                serial: '2',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '3',
-                serial: '3',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '4',
-                serial: '4',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '5',
-                serial: '5',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '6',
-                serial: '6',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '7',
-                serial: '7',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-            {
-                key: '8',
-                serial: '8',
-                name: 'Mike',
-                email: 'email@gmail.com',
-                date: '11 oct 2024, 11:00PM',
-            },
-
-        ];
+        {
+            key: '1',
+            serial: '1',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '2',
+            serial: '2',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '3',
+            serial: '3',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '4',
+            serial: '4',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '5',
+            serial: '5',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '6',
+            serial: '6',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '7',
+            serial: '7',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+        {
+            key: '8',
+            serial: '8',
+            name: 'Mike',
+            email: 'email@gmail.com',
+            date: '11 oct 2024, 11:00PM',
+        },
+    ];
 
     const columns: {
         title: string;
         dataIndex: string;
         key: string;
-        align: string;
+        align?: 'left' | 'right' | 'center'; // Specify valid values for align
     }[] = [
-            {
-                title: 'Serial',
-                dataIndex: 'serial',
-                key: 'serial',
-                align: 'center',
-            },
-            {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-                align: 'center',
-            },
-            {
-                title: 'Email',
-                dataIndex: 'email',
-                key: 'email',
-                align: 'center',
-            },
-            {
-                title: 'Date',
-                dataIndex: 'date',
-                key: 'date',
-                align: 'center',
-            },
-        ];
-
+        {
+            title: 'Serial',
+            dataIndex: 'serial',
+            key: 'serial',
+            align: 'center',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            align: 'center',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+            align: 'center',
+        },
+        {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+            align: 'center',
+        },
+    ];
 
     return (
-        <div className=" bg-[#f8f8f8] pb-10 rounded-t-lg rounded-b-md">
+        <div className="bg-[#f8f8f8] pb-10 rounded-t-lg rounded-b-md">
             <ConfigProvider
                 theme={{
                     components: {
@@ -122,16 +117,15 @@ const DashboardTable = () => {
                     },
                 }}
             >
-                
-                <Table className=" rounded-md table-custom-radius" align="center" style={{}} pagination={{pageSize: 6}}  dataSource={dataSource} columns={columns}  />
-
+                <Table 
+                    className="rounded-md table-custom-radius" 
+                    pagination={{ pageSize: 6 }} 
+                    dataSource={dataSource} 
+                    columns={columns}  
+                />
             </ConfigProvider>
-
         </div>
     );
 };
-
-// #F0BE1B
-
 
 export default DashboardTable;
